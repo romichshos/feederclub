8eee219b21615b2faaab94ba471318f66c712307
romichshosJS <romichshosjs@gmail.com>
2019-10-05 16:44:21 +0300

    -- feeder club 0.0.4

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import './App.css';
-import Header from './Header/Header';
-import Menu from './NavBar/Menu';
+// import './App.css';
+import Header from './Header/Header.jsx';
+// import Menu from './NavBar/Menu';
 import { BrowserRouter as Router,  } from 'react-router-dom';
+/*
 import { Switch, Route } from 'react-router';
 import Contacts from "./Contacts/Contacts";
 import MainPage from "./MainPage/MainPage";
 import About from "./About/About";
 import MyGallery from "./Galery/Galery";
+*/
 
 const App =()=>(
    <Router>
     <div className="container">
-      <Header />
-    <div>
+      <Header/>
+{/*    <div>
         <Route exact path='/' component={MainPage}/>
         <Route path='/Contacts' component={Contacts}/>
         <Route path='/MyGallery' component={MyGallery}/>
         <Route path='/About' component={About}/>
-    </div>
+    </div>*/}
     </div>
    </Router>
     )
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import Menu from '../NavBar/Menu';
 import PropTypes from 'prop-types';
+import './Header.css';
 
-const Header =()=>(
-       <header className="header">
-         <div className="header-logo">
-             <a href="/fc_logo.svg">
-             </a>
-         </div>
-         <div className="header-title">
-           <h1>Шосткинский фидерный клуб</h1>
-         </div>
-        <Menu />
-        </header>
-    )
+const Header = () => (
+    <header className="header">
+        <div className="header_wrap wrap">
+            <div className="header_top">
+                <div className="header_logo logo">
+                    <a className="logo_link" href="/fc_logo.svg">
+                    </a>
+                </div>
+                <div className="header_title">
+                    <h3>Шосткинский</h3>
+                    <h3>Фидерный</h3>
+                    <h3>Клуб</h3>
+                </div>
+                <Menu/>
+            </div>
+        </div>
+    </header>
+)
 
 export default Header
