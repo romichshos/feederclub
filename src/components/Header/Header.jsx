f318a41cf8ca0d66dd8865338e4d63c65abe4afc
romichshosJS <romichshosjs@gmail.com>
2019-10-07 23:47:19 +0300

    -- feeder club 0.0.9

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,11 +12,11 @@ const Header = () => (
                       <img className='logo_img' src='http://localhost:3000/fc_logo.png' alt='Логотип'/>
                     </a>
                 </div>
-                <div className="header_title">
+        {/*        <div className="header_title">
                     <h3><span className="capital_letter">Ш</span>осткинский</h3>
                     <h3><span className="capital_letter">Ф</span>идерный</h3>
                     <h3><span className="capital_letter">К</span>луб</h3>
-                </div>
+                </div>*/}
                 <Menu/>
             </div>
             <div className="header_info">
