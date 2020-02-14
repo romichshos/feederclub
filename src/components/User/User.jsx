38984dd88f848ed6a0528a3b8983171933dc8170
romichshosJS <romichshosjs@gmail.com>
2020-02-14 19:34:36 +0200

    -- 14_02

---
+++ src/components/User/User.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Link} from 'react-scroll';
+
+
+const User=(props) => {
+    if (!props.username) {
+    return <Link to="LogForm" spy={true} smooth={true} offset={-400} duration={2500} className= "menu_item" onClick={props.onClickLogIn}>Войти
+    </Link>;
+}
+return <Link to="" spy={true} smooth={true} offset={-400} duration={2500} className="menu_item menu_item_login">
+    Привет  {props.username} !
+</Link>;
+}
+
+)
+
+export default User
