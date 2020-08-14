173b7481ca43645f090222ec7062cff1e5a7413f
romichshosJS <romichshosjs@gmail.com>
2020-08-14 23:33:50 +0300

    - send mailing - body is not empty! don't use useEffect

--- config/config.js
+++ config/config.js
@@ -2,7 +2,8 @@
 exports.cfg = {
             dbConnect:  'mongodb://localhost:27017/mongoose_auth',
             serverPort: 8000,
-            serverPortMailer: 3002
+            serverPortMailer: 3002,
+            APP_PRIVATE_IP_ADRESS: '31.131.18.2'
 
 }
 
--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,33 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Contacts.css';
 import Footer from '../Footer/Footer.jsx';
 import {Element} from 'react-scroll';
 import {useForm} from 'react-hook-form';
 
 
-
 const Contacts = () => {
-    const {register, handleSubmit, errors, reset } = useForm({defaultValues: {
-             name: "test-user",
-            email: "rkostik@ukr.net",
-             text: "I'm test"}
-    }
-);
-    const onSubmit = (data, e) => {console.log(data);
-        reset({});
-    };
+     const {register, handleSubmit, errors, reset } = useForm({defaultValues: {
+              name: "test-user",
+             email: "rkostik@ukr.net",
+              text: "I'm test"}
+     }
+ );
+     const onSubmit = async (data) => {
+              console.log(data);
+              const result = await fetch(`http://localhost:3002/send`, {
+                 method: 'POST',
+                 body: JSON.stringify(data),
+                 headers: {
+                     'Content-Type': 'application/json'
+                 }
+             });
+             reset({});
+         };
 
-  useEffect(async (data) => {
-         const result = await fetch('http://localhost:3002/send', {
-            method: 'POST',
-            body:JSON.stringify(data),
-            headers: {
-                'Content-Type' : 'application/json'
-            }
-        });
-        reset(result); // asynchronously reset your form values
-    }, [reset])
- return   (
+    return   (
         <div className="contacts_container">
             <div className="contacts_header">
                 <div className="contacts_div">
@@ -36,21 +33,21 @@ const Contacts = () => {
                 </div>
                 <div className="contacts_wrap">
                     <form name="callback_form" method="post" onSubmit={handleSubmit(onSubmit)}>
-                        <div className="form_input">
-                            <p><span className="span_header">Имя <span className="red">*</span></span>
-                                <input ref={register({ required: true })} name="name" type="text"/></p>
-                                {errors.name && 'Введите имя'}
-                            <p>Email <span className="red">*</span>
-                                <input ref={register({ required: true })} name="email" type="text"/></p>
-                            {errors.email && 'Введите EMAIL'}
-                        </div>
-                        <p>Сообщение <span className="red">*</span></p>
-                        <textarea ref={register({ required: true })} name='text'></textarea>
-                        {errors.text && 'Введите текст'}
-                        <div className="btn_block">
-                            <input className="input_btn" type="submit" value="Отправить"/>
-                        </div>
-                    </form>
+                    <div className="form_input">
+                        <p><span className="span_header">Имя <span className="red">*</span></span>
+                            <input ref={register({ required: true })} name="name" type="text"/></p>
+                           {errors.name && 'Введите имя'}
+                        <p>Email <span className="red">*</span>
+                            <input ref={register({ required: true })} name="email" type="text"/></p>
+                         {errors.email && 'Введите EMAIL'}
+                    </div>
+                    <p>Сообщение <span className="red">*</span></p>
+                    <textarea ref={register({ required: true })} name='text'></textarea>
+                     {errors.text && 'Введите текст'}
+                    <div className="btn_block">
+                        <input className="input_btn" type="submit" value="Отправить"/>
+                    </div>
+                 </form>
                 </div>
             </div>
             <div className="contacts_content">
